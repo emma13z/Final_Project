ac0164ef5d2b89f6c64bc988bea9fdaf25526620
Emma Zaremba <emmazaremba13@gmail.com>
2023-11-01 14:29:55 +0200

    started the JS part: validation of the form

---
+++ index.js
@@ -0,0 +1,72 @@
+document.addEventListener (`DOMContentLoaded`, function () {
+const form = document.getElementById('form');
+const inputName = document.getElementById('inputName');
+const outputName = document.getElementById('outputName');
+const inputEmail = document.getElementById('inputEmail');
+const outputEmail = document.getElementById('outputEmail');
+const inputDate= document.getElementById('inputDate');
+const outputDate = document.getElementById('outputDate');
+const inputTime= document.getElementById('inputTime');
+const outputTime = document.getElementById('outputTime');
+const inputMessage = document.getElementById('inputMessage');
+const outputMessage = document.getElementById('outputMessage');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const name = inputName.value.trim();
+
+  if (name.length < 3 ) {
+    outputName.textContent = `Please write your name.`;
+    outputName.style.color = `red`;
+  } else {
+    outputName.textContent = `Hello, ${name}!`;
+    outputName.style.color =`green`;
+  }
+
+  const email = inputEmail.value;
+
+  if (email === `` ) {
+     outputEmail.textContent = `Please write your email.`;
+     outputEmail.style.color = `red`;
+  } else {
+    outputEmail.textContent = `Well received`;
+    outputEmail.style.color =`green`;
+  }
+
+  const date = inputDate.value;
+
+  if (date === `` ) {
+     outputDate.textContent = `Please select preferred date`;
+     outputDate.style.color = `red`;
+  } else {
+    outputDate.textContent = `Well received`;
+    outputDate.style.color =`green`;
+  }
+
+  const time = inputTime.value;
+  
+   if (time === `` ) {
+       outputTime.textContent = `Please select preferred time`;
+       outputTime.style.color = `red`;
+  } else {
+      outputTime.textContent = `Well received`;
+      outputTime.style.color =`green`;
+  }
+
+  const message = inputMessage.value; 
+
+  if (message.length < 12) {
+     outputMessage.textContent = `Please write the additional notes`;
+     outputMessage.style.color = `red`;
+  } else {
+    outputMessage.textContent = `Thank you for the comments!`;
+    outputMessage.style.color =`green`;
+  }
+
+
+
+}
+})
